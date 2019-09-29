4c4ac1b3c73200728f1f56193fe9905aa391abd4
Dobrica Pavlinusic <dpavlin@rot13.org>
2019-09-29 11:40:15 +0200

    check crc

--- pms3003.pl
+++ pms3003.pl
@@ -5,9 +5,11 @@ use strict;
 use Device::SerialPort;
 use Data::Dump qw(dump);
 
-my $port = shift @ARGV || '/dev/ttyUSB1';
+my $port = shift @ARGV || '/dev/ttyUSB0';
 my $influx_url = shift @ARGV || 'http://10.13.37.229:8186/write?db=telegraf';
 $influx_url = 'http://10.13.37.92:8086/write?db=rot13';
+
+my $debug = $ENV{DEBUG} || 0;
 
 my $s = new Device::SerialPort( $port ) || die $!;
 $s->baudrate(9600);
@@ -36,11 +38,19 @@ while (1) {
 	my ($len, $string) = $s->read(24);
 	if ( $len > 0 ) {
 		my @v = unpack('n*', $string);
-		warn "# $len ",dump($string), dump( @v ), $/;
+		warn "# $len ",dump($string), dump( @v ), $/ if $debug;
 		my $header = shift @v; 
-		if ( $header == 0x424d ) {
+		my $checksum = pop @v;	  # remove checksum
+	
+		my $sum = 0;
+		foreach my $b ( unpack('C*', substr($string,0,-2) ) ) {
+			$sum += $b;
+		}
+		$sum = $sum & 0xffff;
+
+		if ( $header == 0x424d && $sum == $checksum ) {
 			shift @v; # skip len
-			pop @v;	  # remove checksum
+
 			my $influx = "pms3003,dc=trnjanska ";
 			foreach my $i ( 0 .. $#v ) {
 				$influx .= "$names[$i]=$v[$i],";
