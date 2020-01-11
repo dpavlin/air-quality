054aef4ee589b0826c7fc1ec8295bd50684a4390
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-01-11 09:20:39 +0100

    better filter for sox output

--- audio2mqtt.sh
+++ audio2mqtt.sh
@@ -4,7 +4,7 @@ while true ; do
 
 arecord -d 1 -r 96000 /dev/shm/tmp.wav
 time=`date +'%s%N'`
-info=`sox /dev/shm/tmp.wav -n stat 2>&1 | sed -e 's/(.*)//' -e 's/: */=/' -e 's/  */_/g' | tr '\n' ',' | sed 's/,$//'`
+info=`sox /dev/shm/tmp.wav -n stat 2>&1 | grep '[0-9][0-9][0-9]*$' | sed -e 's/[()]//g' -e 's/: */=/' -e 's/  */_/g' | tr '\n' ',' | sed 's/[, ]*$//'`
 curl --silent -XPOST http://10.60.0.92:8086/write?db=rot13 --data-binary "fan,dc=trnjanska $info $time"
 
 done
