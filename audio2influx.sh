42b7874fd94e776acce9f6fcbea694474d848368
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-02-19 21:51:28 +0100

    added consistency=any

--- audio2influx.sh
+++ audio2influx.sh
@@ -5,6 +5,6 @@ while true ; do
 arecord -d 1 -r 96000 /dev/shm/tmp.wav 2>/dev/null
 time=`date +'%s%N'`
 info=`sox /dev/shm/tmp.wav -n stat 2>&1 | grep '[0-9][0-9][0-9]*$' | sed -e 's/[()]//g' -e 's/: */=/' -e 's/  */_/g' | tee /dev/shm/audio | tr '\n' ',' | sed 's/[, ]*$//'`
-curl --silent -XPOST http://10.60.0.92:8086/write?db=rot13 --data-binary "fan,dc=trnjanska $info $time"
+curl --silent -XPOST 'http://10.60.0.92:8086/write?consistency=any&db=rot13' --data-binary "fan,dc=trnjanska $info $time"
 
 done
--- pms3003.pl
+++ pms3003.pl
@@ -7,8 +7,7 @@ use Time::HiRes;
 use Data::Dump qw(dump);
 
 my $port = shift @ARGV || '/dev/ttyUSB0';
-my $influx_url = shift @ARGV || 'http://10.13.37.229:8186/write?db=telegraf';
-$influx_url = 'http://10.13.37.92:8086/write?db=rot13';
+my $influx_url = shift @ARGV || 'http://10.13.37.92:8086/write?consistency=any&db=rot13';
 
 my $debug = $ENV{DEBUG} || 0;
 
