2f2e6dc5e553b73e4e3f278f764194ef34ff1c29
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-04-12 11:53:03 +0200

    move arduino with mq sensors to debee

--- mq.sh
+++ mq.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-MEASUREMENT=mq /home/dpavlin/air-quality/dsm501.pl /dev/serial/by-path/pci-0000:00:1a.7-usb-0:5.3.2:1.0-port0
+MEASUREMENT=mq /home/dpavlin/air-quality/dsm501.pl /dev/serial/by-path/pci-0000:00:1d.0-usb-0:2:1.0-port0 'http://10.60.0.92:8086/write?consistency=any&db=rot13' # debee
