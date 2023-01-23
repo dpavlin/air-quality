d50681e1da5d929ba38772d7edd6782380106996
Dobrica Pavlinusic <dpavlin@rot13.org>
2023-01-23 17:10:54 +0100

    don't emit readings so systemd doesn't have to log it

--- ble-mijia.sh
+++ ble-mijia.sh
@@ -9,7 +9,7 @@ gatttool -b $mac --char-write-req --handle='0x0038' --value="0100" --listen | te
 
 v=$( echo $bt | awk '{ print "temperature=" strtonum("0x"$7$6) / 100, "humidity=" strtonum("0x"$8), "a=" strtonum("0x"$9), "b="strtonum("0x"$10) }' | sed 's/ /,/g' )
 
-echo $(date +%Y-%m-%d\ %H:%M:%S) $mac $v
+#echo $(date +%Y-%m-%d\ %H:%M:%S) $mac $v
 
 curl --silent -XPOST 'http://10.60.0.92:8086/write?consistency=any&db=rot13' --data-binary "mijia,dc=trnjanska,mac=$mac $v"
 
